6bed9910a70c31bca9ee1b568ac049c71586c914
joshlacal <joshlacal@gmail.com>
2024-01-19 13:38:35 -0500

    Initial Commit

---
+++ ATProtoKit/ATProtoKit.h
@@ -0,0 +1,18 @@
+//
+//  ATProtoKit.h
+//  ATProtoKit
+//
+//  Created by Josh LaCalamito on 1/19/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ATProtoKit.
+FOUNDATION_EXPORT double ATProtoKitVersionNumber;
+
+//! Project version string for ATProtoKit.
+FOUNDATION_EXPORT const unsigned char ATProtoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATProtoKit/PublicHeader.h>
+
+
